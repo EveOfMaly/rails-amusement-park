569c067ef6fc90f1eb404d715bc98d40ef37cfcb
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-03-24 17:55:52 -0400

    Completed attraction test

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -4,16 +4,19 @@ class Ride < ActiveRecord::Base
     belongs_to :attraction
 
     def take_ride 
-        if user.tickets < attraction.tickets
+        if (user.tickets < attraction.tickets) && (user.height < attraction.min_height)
+            "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+        elsif user.tickets < attraction.tickets
             "Sorry. You do not have enough tickets to ride the #{attraction.name}." 
         elsif user.height < attraction.min_height 
             "Sorry. You are not tall enough to ride the #{attraction.name}."
-        elsif (user.tickets < attraction.tickets) && (user.height < attraction.min_height)
-            "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
         else 
             new_ticket_number = user.tickets - attraction.tickets 
-            user.update(name: user.name, password_digest: user.password_digest, nausea: user.nausea, happiness: user.happiness, tickets: new_ticket_number, height: user.height, admin: user.admin)
+            new_nausia = user.nausea + attraction.nausea_rating
+            new_happiness = user.happiness + attraction.happiness_rating
+            user.update(name: user.name, password_digest: user.password_digest, nausea: new_nausia, happiness: new_happiness, tickets: new_ticket_number, height: user.height, admin: user.admin)
             #update the ride
+            user
         end
 
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-    validates :name, :password, presence: true
+    validates :name, :password_digest, presence: true
 
     has_secure_password 
     
